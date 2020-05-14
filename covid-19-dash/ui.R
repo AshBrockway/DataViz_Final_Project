76797c8ea78ca5ab09525ddeafb650b807a40726
mandab749 <54584390+mandab749@users.noreply.github.com>
2020-05-14 11:16:55 -0400

    Update ui.R
    changes adding world map

--- covid-19-dash/ui.R
+++ covid-19-dash/ui.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(shinydashboard)
 library(readr)
+​
 filePath = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
 dff10 <- read_csv(filePath) %>% filter(state == "Florida")
 dashboardPage(
@@ -16,6 +17,10 @@ dashboardPage(
                         solidHeader = TRUE,
                         DTOutput("mydata")
                     )                
+            ),
+		tabItem(
+                tabName = "WorldMap1",
+                plotlyOutput("world_plot", width = "1800px", height = "800px")
             ))
     )
 )
