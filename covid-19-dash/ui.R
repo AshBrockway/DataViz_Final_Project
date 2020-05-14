0e6983e8e7c29d8316a93d271a035785b8924c5c
natewagner10 <54585457+natewagner10@users.noreply.github.com>
2020-05-14 11:59:54 -0400

    Update ui.R

--- covid-19-dash/ui.R
+++ covid-19-dash/ui.R
@@ -1,10 +1,8 @@
-library(shiny)
 library(shinydashboard)
 library(readr)
 library(tidyverse)
 library(DT)
 library(plotly)
-
 filePath = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
 dff10 <- read_csv(filePath) %>% filter(state == "Florida")
 dashboardPage(
@@ -12,24 +10,37 @@ dashboardPage(
     dashboardSidebar(),
     dashboardBody(
         tabItems(tabItem(
-            tabName = "TAB1", 
-            fluidRow(
-                box(width=6, height = 550,
-                    status="info",
-                    title = "Data from New York Times",  
+                tabName = "TAB1", 
+        fluidRow(
+                valueBoxOutput("vbox", width = 6), 
+                valueBoxOutput("vbox2", width = 6)
+            ),
+                fluidRow(
+            box(width=6,  height = 550,
+                    status="info", 
+                    title="FL County-Level Covid-19 Cases over Time",
                     solidHeader = TRUE,
-                    DTOutput("mydata"),
+                    plotOutput("myplot")
+          ),
+                    box(width=6, height = 550,
+                        status="info",
+                        title = "Data from New York Times",  
+                    solidHeader = TRUE,
+                        DTOutput("mydata"),
+            box(width=6, height = 550,
+                    status="info",
+                    imageOutput("annomate")),
+                solidHeader = TRUE,
                     box(width = 6, height = 550,
                         status = "info",
                         plotlyOutput("bar")),
                     solidHeader = TRUE
                 )
-                
+                                  
             ),
-            tabItem(
+        tabItem(
                 tabName = "WorldMap1",
                 plotlyOutput("world_plot", width = "1800px", height = "800px")
             ))
-        )
     )
 )
